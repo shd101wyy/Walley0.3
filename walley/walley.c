a7e626507cccddd8477f7166a29179247472259d
Yiyi Wang <shd101wyy@sina.com>
2014-04-05 00:38:33 -0500

    huhh

--- walley/walley.c
+++ walley/walley.c
@@ -84,7 +84,7 @@ struct Object {
       char resizable;  // is this vector resizable
     } Vector;
     struct {          // builtin lambda
-      Object* (*func_ptr)(Object*); // function pointer
+      Object* (*func_ptr)(Object*, int param_num); // function pointer
     } Builtin_Lambda;
   } data;
 };
@@ -93,6 +93,7 @@ struct Object {
 
 #define vector_Get(v_, index)  ((v_)->data.Vector.v[(index)])
 #define vector_Length(v) ((v)->data.Vector.length)
+#define vector_Size(v) ((v)->data.Vector.size)
 #define vector_Set(v_, index, o_) ((v_)->data.Vector.v[(index)] = o_)
 #define vector_set_builtin_lambda(v_, index, o_) ((v_)->data.Vector.v[(index)] = Object_initBuiltinLambda(o_))
 #define string_Length(v_) ((v_)->data.String.length)
@@ -187,7 +188,7 @@ Object * Object_initUserDefinedLambda(char param_num, char variadic_place, int s
   o->data.User_Defined_Lambda.env = env;
   return o;
 }
-Object * Object_initBuiltinLambda(Object* (*func_ptr)(Object *)){
+Object * Object_initBuiltinLambda(Object* (*func_ptr)(Object *, int)){
   Object * o = allocateObject();
   o->type = BUILTIN_LAMBDA;
   o->data.Builtin_Lambda.func_ptr = func_ptr;
@@ -216,17 +217,21 @@ void addValueToEnvironment(Object * env, Object * value, int m, int n){
 /*
   builtin lambdas
 */
-Object *builtin_cons(Object * params){
+// 0 cons
+Object *builtin_cons(Object * params, int param_num){
   return cons(vector_Get(params, 2), // params 0 1 are pc and env
               vector_Get(params, 3));
 }
-Object *builtin_car(Object * params){
+// 1 car
+Object *builtin_car(Object * params, int param_num){
   return car(vector_Get(params, 2));
-}
-Object *builtin_cdr(Object * params){
+} 
+// 2 cdr 
+Object *builtin_cdr(Object * params, int param_num){
   return cdr(vector_Get(params, 3));
 }
-Object *builtin_add(Object * params){
+// 3 +
+Object *builtin_add(Object * params, int param_num){
   if(vector_Get(params, 2)->type == INTEGER && vector_Get(params, 3)->type == INTEGER)
     return Object_initInteger(vector_Get(params, 2)->data.Integer.v + 
                               vector_Get(params, 3)->data.Integer.v);
@@ -234,7 +239,8 @@ Object *builtin_add(Object * params){
     return Object_initDouble(vector_Get(params, 2)->data.Double.v + 
                               vector_Get(params, 3)->data.Double.v);
 }
-Object *builtin_sub(Object * params){
+// 4 - 
+Object *builtin_sub(Object * params, int param_num){
   if(vector_Get(params, 2)->type == INTEGER && vector_Get(params, 3)->type == INTEGER)
     return Object_initInteger(vector_Get(params, 2)->data.Integer.v - 
                               vector_Get(params, 3)->data.Integer.v);
@@ -242,7 +248,8 @@ Object *builtin_sub(Object * params){
     return Object_initDouble(vector_Get(params, 2)->data.Double.v -
                               vector_Get(params, 3)->data.Double.v);
 }
-Object *builtin_mul(Object * params){
+// 5 *
+Object *builtin_mul(Object * params, int param_num){
   if(vector_Get(params, 2)->type == INTEGER && vector_Get(params, 3)->type == INTEGER)
     return Object_initInteger(vector_Get(params, 2)->data.Integer.v * 
                               vector_Get(params, 3)->data.Integer.v);
@@ -250,7 +257,8 @@ Object *builtin_mul(Object * params){
     return Object_initDouble(vector_Get(params, 2)->data.Double.v * 
                               vector_Get(params, 3)->data.Double.v);
 }
-Object *builtin_div(Object * params){
+// 6 /
+Object *builtin_div(Object * params, int param_num){
   if(vector_Get(params, 2)->type == INTEGER && vector_Get(params, 3)->type == INTEGER)
     return Object_initInteger(vector_Get(params, 2)->data.Integer.v / 
                               vector_Get(params, 3)->data.Integer.v);
@@ -258,7 +266,174 @@ Object *builtin_div(Object * params){
     return Object_initDouble(vector_Get(params, 2)->data.Double.v / 
                               vector_Get(params, 3)->data.Double.v);
 }
-
+// 7 vector!
+Object *builtin_vector(Object * params, int param_num){
+  int size = (param_num / 16 + 1) * 16; // determine the size
+  int i = 0;
+  Object * v = Object_initVector(1, size);
+  for(; i < param_num; i++){
+    vector_Get(v, i) = vector_Get(v, i + 2);
+  }
+  v->data.Vector.length = param_num;
+  return v;
+}
+// 8 vector
+Object *builtin_vector_with_unchangable_length(Object * params, int param_num){
+  int i = 0;
+  Object * v = Object_initVector(0, param_num);
+  for(; i < param_num; i++){
+    vector_Get(v, i) = vector_Get(v, i + 2);
+  }
+  v->data.Vector.length = param_num;
+  return v;
+}
+// 9 vector-length
+Object *builtin_vector_length(Object * params, int param_num){
+  return Object_initInteger(vector_Length(vector_Get(params, 2)));
+}
+// 10 vector-push!
+Object *builtin_vector_push(Object * params, int param_num){
+  int i = 0;
+  Object * vec = vector_Get(params, 2);
+  int length = vector_Length(vec);
+  int size = vector_Size(vec);
+  for(i = 0; i < param_num; i++){
+    if(length == size){
+      if(vec->data.Vector.resizable){
+        vec->data.Vector.v = realloc(vec->data.Vector.v, sizeof(Object*) * (size * 2)); // increase size
+        vec->data.Vector.size *= 2;
+      }
+      else{
+        printf("ERROR: Cannot change size of vector\n");
+        return Object_initNull();
+      }
+    }
+    vector_Set(vec, length, vector_Get(params, 3 + i));
+    length++;
+  }
+  vector_Get(params, 2)->data.Vector.length = length;
+  return vector_Get(params, 2);
+}
+// 11 vector-pop!
+Object *builtin_vector_pop(Object * params, int param_num){
+    Object * vec = vector_Get(params, 2);
+    int length = vector_Length(vec);
+    length = length - 1;
+    vec->data.Vector.length = length;
+    return vector_Get(vec, length);
+}
+// 12 =
+Object *builtin_num_equal(Object * params, int param_num){
+  Object * param0 = vector_Get(params, 2);
+  Object * param1 = vector_Get(params, 3);
+  if(param0->type == INTEGER ? param0->data.Integer.v : param0->data.Double.v 
+     == 
+     param1->type == INTEGER ? param1->data.Integer.v : param1->data.Double.v){
+    return Object_initInteger(1);
+  }
+  return Object_initNull();
+}
+// 13 <
+Object *builtin_num_lt(Object * params, int param_num){
+  Object * param0 = vector_Get(params, 2);
+  Object * param1 = vector_Get(params, 3);
+  if(param0->type == INTEGER ? param0->data.Integer.v : param0->data.Double.v 
+     < 
+     param1->type == INTEGER ? param1->data.Integer.v : param1->data.Double.v){
+    return Object_initInteger(1);
+  }
+  return Object_initNull();
+}
+// 14 <=
+Object *builtin_num_le(Object * params, int param_num){
+  Object * param0 = vector_Get(params, 2);
+  Object * param1 = vector_Get(params, 3);
+  if(param0->type == INTEGER ? param0->data.Integer.v : param0->data.Double.v 
+     <= 
+     param1->type == INTEGER ? param1->data.Integer.v : param1->data.Double.v){
+    return Object_initInteger(1);
+  }
+  return Object_initNull();
+}
+// 15 eq?
+Object *builtin_eq(Object * params, int param_num){
+  Object * param0 = vector_Get(params, 2);
+  Object * param1 = vector_Get(params, 3);
+  if((param0->type == INTEGER | param0->type == DOUBLE) && (param1->type == INTEGER | param1->type == DOUBLE)){
+    return builtin_num_equal(params, param_num);
+  }
+  return (param0 == param1) ? Object_initInteger(1) : Object_initNull();
+}
+// 16 string?
+Object *builtin_string_type(Object * params, int param_num){
+  if(vector_Get(params, 2)->type == STRING)
+    return Object_initInteger(1);
+  return Object_initNull();
+}
+// 17 int?
+Object *builtin_int_type(Object * params, int param_num){
+  if(vector_Get(params, 2)->type == INTEGER)
+    return Object_initInteger(1);
+  return Object_initNull();
+}
+// 18 float?
+Object *builtin_float_type(Object * params, int param_num){
+  if(vector_Get(params, 2)->type == DOUBLE)
+    return Object_initInteger(1);
+  return Object_initNull();
+}
+// 19 pair?
+Object *builtin_pair_type(Object * params, int param_num){
+  if(vector_Get(params, 2)->type == PAIR)
+    return Object_initInteger(1);
+  return Object_initNull();
+}
+// 20 null?
+Object *builtin_null_type(Object * params, int param_num){
+  if(vector_Get(params, 2)->type == NULL_)
+    return Object_initInteger(1);
+  return Object_initNull();
+}
+// 21 lambda?
+Object *builtin_lambda_type(Object * params, int param_num){
+  if(vector_Get(params, 2)->type == USER_DEFINED_LAMBDA || vector_Get(params, 2)->type == BUILTIN_LAMBDA)
+    return Object_initInteger(1);
+  return Object_initNull();
+}
+// 22 strcmp  compare string
+Object *builtin_strcmp(Object * params, int param_num){
+  int i = strcmp(vector_Get(params, 2)->data.String.v, vector_Get(params, 3)->data.String.v);
+  return Object_initInteger(i);
+}
+// 23 string-slice
+Object *builtin_string_slice(Object * params, int param_num){
+  char * s = vector_Get(params, 2)->data.String.v;
+  int start = vector_Get(params, 3)->data.Integer.v;
+  int end = vector_Get(params, 4)->data.Integer.v;
+  int length = end - start;
+  char * out = (char*)malloc(sizeof(char) * (length + 1));
+  int i = 0;
+  for(; i < length; i++){
+    out[i] = s[i + start];
+  }
+  out[i] = 0;
+  return Object_initString(out, length);
+}
+// 24 string-length
+Object *builtin_string_length(Object * params, int param_num){
+  return Object_initInteger(string_Length(vector_Get(params, 2)));
+}
+// 25 string-append
+Object *builtin_string_append(Object * params, int param_num){
+  Object * s1 = vector_Get(params, 2);
+  Object * s2 = vector_Get(params, 3);
+  int sum_length = string_Length(s1) + string_Length(s2);
+  char *out = (char*)malloc(sizeof(char)*(sum_length));
+  strcat(out, s1->data.String.v);
+  strcat(out, s2->data.String.v);
+  out[sum_length] = 0;
+  return Object_initString(out, sum_length);
+}
 /*
   create frame0
 */
@@ -273,6 +448,26 @@ Object *createFrame0(){
   vector_set_builtin_lambda(frame, 4, &builtin_sub);
   vector_set_builtin_lambda(frame, 5, &builtin_mul);
   vector_set_builtin_lambda(frame, 6, &builtin_div);
+  vector_set_builtin_lambda(frame, 7, &builtin_vector);
+  vector_set_builtin_lambda(frame, 8, &builtin_vector_with_unchangable_length);
+  vector_set_builtin_lambda(frame, 9, &builtin_vector_length);
+  vector_set_builtin_lambda(frame, 10, &builtin_vector_push);
+  vector_set_builtin_lambda(frame, 11, &builtin_vector_pop);
+  vector_set_builtin_lambda(frame, 12, &builtin_num_equal);
+  vector_set_builtin_lambda(frame, 13, &builtin_num_lt);
+  vector_set_builtin_lambda(frame, 14, &builtin_num_le);
+  vector_set_builtin_lambda(frame, 15, &builtin_eq);
+  vector_set_builtin_lambda(frame, 16, &builtin_string_type);
+  vector_set_builtin_lambda(frame, 17, &builtin_int_type);
+  vector_set_builtin_lambda(frame, 18, &builtin_float_type);
+  vector_set_builtin_lambda(frame, 19, &builtin_pair_type);
+  vector_set_builtin_lambda(frame, 20, &builtin_null_type);
+  vector_set_builtin_lambda(frame, 21, &builtin_lambda_type);
+  vector_set_builtin_lambda(frame, 22, &builtin_strcmp);
+  vector_set_builtin_lambda(frame, 23, &builtin_string_slice);
+  vector_set_builtin_lambda(frame, 24, &builtin_string_length);
+  vector_set_builtin_lambda(frame, 25, &builtin_string_append);
+
 
   return frame;
 }
@@ -322,7 +517,7 @@ Object *VM(int * instructions,
   Object * current_frame_pointer = NULL;
   Object * frames = Object_initVector(0, MAX_STACK_SIZE); // save frames
   Object * new_env;
-  Object * (*func_ptr)(Object*); // function pointer
+  Object * (*func_ptr)(Object*, int); // function pointer
   Object * v;
 
   while(pc != instructions_num){
@@ -374,10 +569,11 @@ Object *VM(int * instructions,
             created_string[i] = 0;
             accumulator = Object_initString(created_string, string_length - 1);
             pc = pc + 1;
+            continue;            
+          default: // null
+            accumulator = Object_initNull();
+            pc = pc + 1;
             continue;
-          default:
-            printf("ERROR: Invalid Constant\n");
-            return NULL;
         }
       case MAKELAMBDA: // make lambda
         param_num = (0x0FC0 & inst) >> 6;
@@ -415,7 +611,7 @@ Object *VM(int * instructions,
           case BUILTIN_LAMBDA: // builtin lambda
             func_ptr = accumulator->data.Builtin_Lambda.func_ptr;
             pc = pc + 1;
-            accumulator = (*func_ptr)(current_frame_pointer); // call function
+            accumulator = (*func_ptr)(current_frame_pointer, param_num); // call function
             
             // pop frame
             frames->data.Vector.length -= 1;
@@ -493,7 +689,7 @@ Object *VM(int * instructions,
   return accumulator;
 }
 
-int insts[12] = {0x5000, 0x2100, 0x0000, 0x0003, 0x6002, 0x2100, 0x0000, 0x0004, 0x6003, 0x1000, 0x0003, 0x7002};
+int insts[12] = {0x2400, 0x9000, 0x0008, 0x2100, 0x0000, 0x0002, 0x8000, 0x0000, 0x0006, 0x2100, 0x0000, 0x0003};
 int main(){
   printf("Walley Language 0.3.673\n");
   Object * o = VM(insts, 12, 0, createEnvironment());
